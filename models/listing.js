79c15efe3ebd55eb20d17703de414574d6b6eb6c
Sumaya shaik <shaikrahamath997@gmail.com>
2025-09-22 12:53:00 +0530

    Initial commit

---
+++ models/listing.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const review = require("./review");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    url: String,
+    filename: String,
+  },
+  price: Number,
+  location: String,
+  country: String,
+  reviews: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  category: {
+    type: String,
+    enum: [
+      "mountain-city",
+      "mountain",
+      "hiking",
+      "pools",
+      "island",
+      "trending",
+      "domes",
+    ],
+  },
+});
+
+listingSchema.post("findOneAndDelete", async (listing) => {
+  if (listing) {
+    await review.deleteMany({ _id: { $in: listing.reviews } });
+  }
+});
+
+const Listing = new mongoose.model("listing", listingSchema);
+module.exports = Listing;
---
+++ routes/listing.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync.js");
+const Listing = require("../models/listing.js");
+const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
+const listingController = require("../controllers/listings.js");
+const multer = require("multer");
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
+
+const reviewRouter = require("./review.js");
+
+router.use("/:id/reviews", reviewRouter);
+
+router.get("/search", listingController.search);
+router.get("/filter", listingController.filter);
+router.get("/new", isLoggedIn, listingController.renderNewForm);
+
+//EDIT Route
+router.get(
+  "/:id/edit",
+  isLoggedIn,
+  isOwner,
+  wrapAsync(listingController.renderEditForm)
+);
+
+router
+  .route("/:id")
+  .get(wrapAsync(listingController.showListing))
+  .put(
+    isLoggedIn,
+    isOwner,
+    upload.single("listing[image]"),
+    validateListing,
+    wrapAsync(listingController.updateListing)
+  )
+  .delete(isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
+
+router.use("/:id/reviews", reviewRouter);
+
+router
+  .route("/")
+  .get(wrapAsync(listingController.index))
+  .post(
+    isLoggedIn,
+    upload.single("listing[image]"),
+    validateListing,
+    wrapAsync(listingController.createListing)
+  );
+
+module.exports = router;
